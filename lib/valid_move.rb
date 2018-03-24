79bdd63ef4da9ef2e0a3f266974ddc4e19ff942d
Marcus Jun <mjun@calstate.edu>
2018-03-24 21:34:45 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 # code your #valid_move? method here
 
-def valid_move? (array,index)
-  if (position_taken?(array,index)&&index.between(0,8))
+def valid_move? (board,index)
+  if (position_taken?(board,index)&&index.between(0,8))
     return true
   else
     return false
@@ -10,10 +10,10 @@ end
 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
-def position_taken? (array,index)
-  if (array[index]==" " || array[index]=="" || array[index]==nil)
+def position_taken? (board,index)
+  if (board[index]==" " || board[index]=="" || board[index]==nil)
     return false
-  elsif(array[index]=="X"||array[index]=="O")
+  elsif(board[index]=="X"||board[index]=="O")
     return true
   end
 end
