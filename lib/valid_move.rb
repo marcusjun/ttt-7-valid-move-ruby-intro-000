c767beab3ddcea67a9fa6edf0aed06aea08cd764
Marcus Jun <mjun@calstate.edu>
2018-03-24 21:34:00 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 # code your #valid_move? method here
 
 def valid_move? (array,index)
-  if (position_taken?&&index.between(0,8))
+  if (position_taken?(array,index)&&index.between(0,8))
     return true
   else
     return false
